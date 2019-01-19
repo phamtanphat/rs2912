727526e17b5127f26058465878d59e1a908c8a0d
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-01-19 16:35:23 +0700

    Update List.js

--- src/Components/List.js
+++ src/Components/List.js
@@ -36,14 +36,14 @@ export default class List extends Component {
         <div className="word-container">
           <h3 className="text-success">{word.en}</h3>
           <h3 className="text-danger">
-            {!word.isMemorized ? '----' : word.vn}
+            {word.isMemorized ? '----' : word.vn}
           </h3>
         </div>
         <div className="btn-container">
           <button
             className={word.isMemorized ? 'btn btn-success' : 'btn btn-danger'}
             onClick={() => this.toggleWord(word.id)}>
-            {word.isMemorized ? 'Memoried' : 'Forgot'}
+            {word.isMemorized ? 'Forgot' : 'Memorized'}
           </button>
           <button
             className="btn btn-warning"
@@ -136,4 +136,3 @@ export default class List extends Component {
     )
   }
 }
-
