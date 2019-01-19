daa6f4a2e486ca8fc668a90692e4b2f75cfea329
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-01-19 14:08:21 +0700

    Update List.js

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Word from './Word';
+// import Word from './Word';
 const words = [
   { id: 'a1', en: "One", vn: "Mot", isMemorized: true },
   { id: 'a2', en: "Two", vn: "Hai", isMemorized: false },
@@ -12,32 +12,37 @@ export default class List extends Component {
     super(props);
     this.state = { words: words };
   }
+  getWorditem(word) {
+    return (
+      <div className="word" key={word.id}>
+        <div className="word-container">
+          <h3 className="text-success">{word.en}</h3>
+          <h3 className="text-danger">
+            {word.isMemorized ? '----' : word.vn}
+          </h3>
+        </div>
+        <div className="btn-container">
+          <button
+            className={word.isMemorized ? 'btn btn-danger' : 'btn btn-success'}>
+            {word.isMemorized ? 'Forgot' : 'Memoried'}
+          </button>
+          <button
+            className="btn btn-warning"
+            onClick={() => {
+              const words = this.state.words.filter(w => w.id !== word.id);
+              this.setState({ words });
+            }}>
+            Remove
+          </button>
+        </div>
+      </div>
+    )
+  }
   render() {
     const { words } = this.state;
     return (
       <div>
-        {words.map(word => (
-          <div className="word" key={word.id}>
-            <div className="word-container">
-              <h3 className="text-success">{word.en}</h3>
-              <h3 className="text-danger">
-                {word.isMemorized ? '----' : word.vn}
-              </h3>
-            </div>
-            <div className="btn-container">
-              <button
-                className={word.isMemorized ? 'btn btn-danger' : 'btn btn-success'}
-                onClick={() => this.toggleWord(word.id)}>
-                {word.isMemorized ? 'Forgot' : 'Memoried'}
-              </button>
-
-              <button className="btn btn-warning"
-                onClick={() => this.removeword(word.id)}>
-                Remove
-            </button>
-            </div>
-          </div>
-        ))}
+        {words.map(word => this.getWorditem(word))}
       </div>
     )
   }
