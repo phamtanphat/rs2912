91e04022f8196b2d9b8a880027965cde04854e6f
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-01-20 11:19:25 +0700

    Update Parent.js

--- src/Components/Parent.js
+++ src/Components/Parent.js
@@ -1,58 +1,22 @@
 import React, { Component } from 'react'
-export default class Parent extends Component {
-    constructor(props){
-        super(props);
-        this.state = {count : 1}
-        this.onIncrease = this.onIncrease.bind(this);
-        this.onDescrease = this.onDescrease.bind(this);
-        this.onReset = this.onReset.bind(this);
-    }
-    onIncrease(){
-        this.setState({count : this.state.count + 1});
-    }
-    onDescrease(){
-        this.setState({count : this.state.count - 1});
-    }
-    onReset(){
-        this.setState({count : 0});
-    }
+import Child from './Child';
+import {connect} from 'react-redux';
+
+class Parent extends Component {
     render() {
         return (
             <div>
-                <h3>Value : {this.state.count} </h3>
-                <Child 
-                    onIncrease={this.onIncrease}
-                    onDescrease={this.onDescrease}
-                    onReset={this.onReset} 
-                />
+                <h3>{`Value : ${this.props.count}`}</h3>
+                <Child />
             </div>
         )
     }
 }
-class Child extends Component {
-    render() {
-       const { onIncrease , onDescrease , onReset } = this.props;
-        return (
-            <div>
-                <button 
-                    className="btn btn-success"
-                    onClick={onIncrease}
-                >
-                    Increase
-                </button>
-                <button 
-                    className="btn btn-danger"
-                    onClick={onDescrease} 
-                >
-                    Descrease
-                </button>
-                <button 
-                    className="btn btn-warning"
-                    onClick={onReset} 
-                >
-                    Reset
-                </button>
-            </div>
-        );
-    }
+
+const mapStateToProps = function(state){
+    return {count : state.count}
 }
+
+export default connect(mapStateToProps)(Parent);
+
+
