4ac9eb712be5546802a0cef33902346045d27809
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-01-20 11:18:58 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 // import Word from './Components/Word'
 import './App.css';
 // import Loop from './Components/Loop'
-import List from './Components/List';
-import Box from './Components/Box';
+// import List from './Components/List';
+// import Box from './Components/Box';
+// import Form from './Components/Form';
+import Parent from './Components/Parent';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore((state = {count : 10} , action) =>{
+    if(action.type === 'INCREASE') return {count : state.count + 1}
+    if(action.type === 'DECREASE') return {count : state.count - 1}
+    if(action.type === 'RESET') return {count : 0}
+    return state;
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <List/>
+      <div>
+          {/* <List/> */}
           {/* <Box/> */}
+          {/* <Form/> */}
+          <Provider store={store}>
+              <Parent/>
+          </Provider>
       </div>
     );
   }
@@ -26,3 +41,8 @@ export default App;
 // es7 react/redux
 // vscode-icons
 
+//Redux
+// Dinh nghia ra store
+// Ket noi store voi components
+// Hien thi store ra components
+// Thay doi store tu components
