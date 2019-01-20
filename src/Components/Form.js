87b7606cc4f83265478e459aa4bef1f67a376b09
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-01-20 11:59:29 +0700

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,71 +1,23 @@
 import React, { Component } from 'react'
-import {connect } from 'react-redux';
-
-class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            txtEn : '',
-            txtVn : '',
-        }
-        this.addWord = this.addWord.bind(this);    
-    }
-   addWord(){
-        const { txtEn , txtVn} = this.state;
-        const word = {
-            id : Math.random(),
-            en : txtEn,
-            vn : txtVn,
-            isMemorized : false
-        }
-        this.setState({txtEn : '' , txtVn : ''})
-        this.props.dispatch({type : 'ADD_WORD' , word});
-    }
+import {connect} from 'react-redux';
+class Filter extends Component {
     render() {
-        const { shouldShowForm , dispatch } = this.props;
+        const { filterMode , dispatch} = this.props;
         return (
             <div>
-                {shouldShowForm ? 
-                    <div className="form-group word-from" >
-                        <input
-                            placeholder="English"
-                            className="form-control"
-                            value={this.state.txtEn}
-                            onChange={evt => this.setState({txtEn : evt.target.value})}/>
-                        <br />
-                        <input
-                            placeholder="Vietnamese"
-                            className="form-control"
-                            value={this.state.txtVn}
-                            onChange={evt => this.setState({txtVn : evt.target.value})}/>
-                        <br />
-                        <div className="btn-container">
-                            <button 
-                                className="btn btn-success"
-                                onClick={this.addWord}>
-                                Add word
-                            </button>
-                            <button
-                                className="btn btn-danger"
-                                onClick={() =>dispatch({type : "TOGGLE_FORM"})}>
-                                Cancel
-                            </button>
-                        </div>
-                    </div> :
-                     <button 
-                     className="btn btn-success"
-                     style={{width : 200 ,margin : 10}}
-                     onClick={() =>dispatch({type : "TOGGLE_FORM"})}>
-                             +
-                    </button>
-                }
+                <select
+                    className="word"
+                    value={filterMode}
+                    onChange={evt => dispatch({type : 'SET_FILTER_MODE' , filterMode : evt.target.value})}>
+                    <option value="Show_All">Show All</option>
+                    <option value="Show_Memorized">Show Memoried</option>
+                    <option value="Show_Forgot">Show Forgot</option>
+                </select>
             </div>
         )
     }
 }
-
 const mapStateToProps = function(state){
-    return {shouldShowForm : state.shouldShowForm};
+    return {filterMode : state.filterMode}
 }
-
-export default connect(mapStateToProps)(Form);
+export default connect(mapStateToProps)(Filter);
