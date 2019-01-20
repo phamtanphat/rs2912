f8dedee4a5c3f0f1e03f935f78614b7b7aa773a4
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-01-20 10:37:09 +0700

    Update Filter.js

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 
 export default class Filter extends Component {
     render() {
-        const { filterMode } = this.props;
+        const { filterMode , onFilter} = this.props;
         return (
             <div>
                 <select
                     className="word"
-                    value={filterMode}>
+                    value={filterMode}
+                    onChange={evt => onFilter(evt.target.value)}>
                     <option value="Show_All">Show All</option>
                     <option value="Show_Memorized">Show Memoried</option>
                     <option value="Show_Forgot">Show Forgot</option>
