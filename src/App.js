75f853bf5a1954f9e921bc4c0cee6df8d1f3439c
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-01-26 14:19:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
 import React, { Component } from 'react';
-// import Word from './Components/Word'
 import './App.css';
-// import Loop from './Components/Loop'
 import List from './Components/List';
-// import Box from './Components/Box';
-// import Form from './Components/Form';
-// import Parent from './Components/Parent';
-import { createStore , combineReducers} from 'redux';
+import {store} from './Components/redux/store';
 import { Provider } from 'react-redux';
-
-const words = [
-  { id: 'a1', en: "One", vn: "Mot", isMemorized: true },
-  { id: 'a2', en: "Two", vn: "Hai", isMemorized: false },
-  { id: 'a3', en: "Three", vn: "Ba", isMemorized: false },
-  { id: 'a4', en: "Four", vn: "Bon", isMemorized: true },
-]
-
-function wordReducer(state = words, action) {
-  if (action.type === 'REMOVE_WORD') {
-    const words = state.filter(w => w.id !== action.id)
-    return words;
-  }
-  if (action.type === 'ADD_WORD') {
-    const words = state.concat(action.word);
-    return words;
-  }
-  if (action.type === 'TOGGLE_WORD') {
-    const words = state.map(w => {
-      if (action.id === w.id) return { ...w, isMemorized: !w.isMemorized }
-      return w;
-    })
-    return words;
-  }
-  return state;
-}
-function shouldShowFormReducer(state = false, action) {
-  if (action.type === 'TOGGLE_FORM') return !state;
-  if (action.type === 'ADD_WORD') return false;
-  return state;
-}
-function filterModeReducer(state = false, action) {
-  if (action.type === 'SET_FILTER_MODE') return action.filterMode
-  return state;
-}
-const reducer = combineReducers({
-    words : wordReducer,
-    shouldShowForm : shouldShowFormReducer,
-    filterMode : filterModeReducer
-})
-const store = createStore(reducer);
 
 class App extends Component {
   render() {
